ee7b36d51722486061ff28c28ab1f705816a5578
hahahayesss <hahahayesss@gmail.com>
2018-11-28 20:22:42 +0300

    Version 0.01

--- app/src/main/java/com/r00t/v_lib/MVPTestActivity.java
+++ app/src/main/java/com/r00t/v_lib/MVPTestActivity.java
@@ -1,6 +1,7 @@
 package com.r00t.v_lib;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,8 +12,12 @@ public abstract class MVPTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvptest);
 
-        findViewById(R.id.testButton)
-                .setOnClickListener(v -> onClicked());
+        findViewById(R.id.testButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClicked();
+            }
+        });
     }
 
     protected abstract void onClicked();
